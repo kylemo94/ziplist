5e20cd9340ecf5aa20ff98d0349fa33151239e8f
Kyle Okamoto <46773190+kylemo94@users.noreply.github.com>
2019-02-04 21:29:30 -1000

    Finished ziplist

---
+++ ziplist.js
@@ -0,0 +1,21 @@
+function ziplist(arrayOne, arrayTwo){
+  let newArray =[];
+  let i = 0;
+  while(i<=arrayTwo.length-1){
+    newArray.push(arrayOne[i]);
+    newArray.push(arrayTwo[i]);
+    i++;
+  }
+  return newArray;
+}
+let arrOne = [1, 2, 3];
+let arrTwo = ['a', 'b', 'c'];
+
+console.log(ziplist(arrOne, arrTwo));
+
+function zipListTheSimpleWay(arrayOne, arrayTwo){
+  let zipArrays = _.zip(arrayOne, arrayTwo);
+  return _.flatten(zipArrays);
+}
+
+console.log(zipListTheSimpleWay(arrOne, arrTwo));
